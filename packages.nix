9d7e3886bbb241153666d1b3c5f4107d574a2f7d
MmeQuignon <matthieu@fwzte.xyz>
2026-08-11 12:28:42 +0200

    Reduce packages managed by home-manager

--- packages.nix
+++ packages.nix
@@ -3,24 +3,15 @@
 
 {
   home.packages = with pkgs; with python3Packages; [
-    pgcli python-lsp-server jq
-    # DBEAVER
-    dbeaver socat # socat required to forward connections from port to socket
-    #
-    imagemagick gimp
-    lastpass-cli # bugged
-    htop dfc arandr mpv-unwrapped
-    patchutils zim
-    # for raumschmiede
-    gnome.networkmanager-openvpn networkmanager-openvpn
-    # recording
-    simplescreenrecorder tdesktop slack keepassxc
-    # docker-compose
-    docker-compose
-
-    # azure stuff
-    #kubectl azure-cli k9s kubelogin
-    #thttpd # for htpasswd
-    sops jq
+    # archive 
+    unzip p7zip fsarchiver zip rar
+    # network
+    nethogs wget rsync curl tcpdump httpie bind
+    # system tools
+    htop dfc sysfsutils file tree compsize
+    # dev tools
+    ripgrep jq
+    # tools
+    imagemagick simplescreenrecorder pastebinit arandr 
   ];
 }
